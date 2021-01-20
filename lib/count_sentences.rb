6f73c87a96d76b54288b2c284d121542a7efbdc9
Zoe Friedman <zoeblair.friedman@gmail.com>
2021-01-20 10:50:56 -0500

    commit tree fdffc2d631cf1070cf91c7365310d8f72b9648c1

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,21 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
+  
   def count_sentences
-
+    # binding.pry
+    array = self.split(/\?|\.|\!/).reject { |s| s.empty? }
+    array.length
   end
 end
